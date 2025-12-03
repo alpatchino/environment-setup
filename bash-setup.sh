5dde49f5440cba74ffe9c38bee605125c7f079d3
alpatchino <mrpatrickmachado@gmail.com>
2025-12-03 22:26:26 +0000

    Update bash-setup.sh

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,21 +1,19 @@
 echo "Starting script..."
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt autoremove # maybe not needed
+sudo apt update && sudo apt upgrade -y   # keep packages current
+sudo apt install build-essential curl git -y  # toolchain
 
-# maybe not needed
-sudo apt-get update
-sudo apt-get upgrade
+# Enable Windows to Linux pasting
+echo "export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0" >> ~/.bashrc
 
-sudo apt-get install -y screenfetch
-sudo apt-get install -y build-essential
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc   # or open a new terminal
 
-# python stuff
-sudo dpkg --configure -a
-sudo apt-get install -y python python-dev python-setuptools
-sudo apt-get install -y python-pip
-sudo apt-get install -y python3-pip
+# 2 · Install the latest LTS and make it default
+nvm install --lts
+nvm alias default node
+node -v && npm -v   # sanity check
 
 echo "Installing ZSH & Oh My Zsh..."
 sudo apt-get install -y zsh
@@ -26,7 +24,7 @@ echo "Download autosuggestion and syntax highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-cp ~/projects/environment-setup/zshrc ~/.zshrc
+cp ~/workspace/environment-setup/zshrc ~/.zshrc
 
 echo "Installing better directory colours...."
 sudo curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
